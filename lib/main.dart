cd8dba5de8515c4ff4668455fee774ce20711109
Vignesh M <ms.vignesh31@gmail.com>
2019-06-01 13:05:02 +0530

    Finished defining routes

--- lib/main.dart
+++ lib/main.dart
@@ -9,19 +9,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Team Upp',
+      initialRoute: '/',
+      routes: {
+        // home screen
+        '/': (context) => TeamUpp(),
+        /**
+         * Created routes here, please enable them as you finish your screen and
+         * change the class name accordingly, don't forget to import the class file first
+         */
+        // Other screen
+        //'/create': (context) => CreateScreen(),
+        // '/details': (context) => DetailsScreen(),
+        // '/login': (context) => LoginScreen(),
+        // '/feed': (context) => FeedScreen(),
+      },
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.purple,
       ),
-      home: TeamUpp(),
     );
   }
 }
